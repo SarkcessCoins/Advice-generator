ab03d8388280616e44f77c32276be01db88cfbee
success uche <successuche46@gmail.com>
2026-09-11 04:12:14 -0700

    disabled the button on loadding state

--- app.js
+++ app.js
@@ -3,21 +3,37 @@ const adviceId = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
 const indicator = document.getElementById("indicator");
 
-async function getQoute() {
+async function getQuote() {
+  if (adviceButton.getAttribute("aria-disabled") === "true") {
+    return;
+  }
+  resetIndicator();
   adviceText.textContent = "Loading. . .";
+  adviceButton.classList.add("disabled");
+  adviceButton.setAttribute("aria-disabled", "true");
+
   try {
     const response = await fetch("https://api.adviceslip.com/advice");
     if (!response.ok) {
       throw new Error(`Error fetching advice: ${response.status}`);
     }
+
     const data = await response.json();
     adviceId.textContent = `${data.slip.id}`;
+    adviceText.style.display = "block";
     adviceText.innerHTML = `<q> ${data.slip.advice} </q>`;
   } catch (error) {
     adviceText.style.display = "none";
     indicator.classList.add("indicator-error");
     indicator.textContent = `${error.message}`;
+  } finally {
+    adviceButton.classList.remove("disabled");
+    adviceButton.setAttribute("aria-disabled", "false");
   }
 }
+function resetIndicator() {
+  indicator.textContent = ` `;
+  indicator.classList.remove("indicator-error");
+}
 
-adviceButton.addEventListener("click", getQoute);
+adviceButton.addEventListener("click", getQuote);
