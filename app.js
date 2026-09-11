bcc93ff8ef03bb9a156418c6b69c246767e06068
success uche <successuche46@gmail.com>
2026-09-11 00:48:39 -0700

    switched to  the async and await fuction

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const adviceButton = document.getElementById("advice-btn");
 const adviceId = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
+const indicator = document.getElementById("indicator");
 
-const handleAdvice = () => {
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.adviceslip.com/advice", true);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      adviceText.textContent = `${data.slip.advice}`;
-      adviceId.textContent = `${data.slip.id}`;
+async function getQoute() {
+  adviceText.textContent = "Loading. . .";
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) {
+      throw new Error(`Error fetching advice: ${response.status}`);
     }
-  };
-  xhr.send();
-};
+    const data = await response.json();
+    adviceId.textContent = `${data.slip.id}`;
+    adviceText.innerHTML = `<q> ${data.slip.advice} </q>`;
+  } catch (error) {
+    adviceText.style.display = "none";
+    indicator.classList.add("indicator-error");
+    indicator.textContent = `${error.message}`;
+  }
+}
 
-adviceButton.addEventListener("click", handleAdvice);
+adviceButton.addEventListener("click", getQoute);
